ce98723b49c483e6ac989a2c9603d13c8a8976fc
viktorstanimirov <viktor.stanimirov@gmail.com>
2024-04-06 14:48:00 +0300

    Addcustom admin for custom user's

--- FastFoodApp/FastFoodApp/accounts/admin.py
+++ FastFoodApp/FastFoodApp/accounts/admin.py
@@ -1,32 +1,14 @@
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import AppUser
+
+from FastFoodApp.accounts.models import AppUser
 
 
 @admin.register(AppUser)
-class CustomUserAdmin(UserAdmin):
-    model = AppUser
-
-    def get_fieldsets(self, request, obj=None):
-        default_fields = sum([list(fields) for name, fields in super().get_fieldsets(request, obj)], [])
-
-        custom_fields = [field.name for field in AppUser._meta.fields if
-                         field.name not in default_fields and field.editable and not field.many_to_many and not field.auto_created]
-
-        fieldsets = super().get_fieldsets(request, obj) + [
-            ('AppUser Fields', {'fields': custom_fields}),
-        ]
-        return fieldsets
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        if not obj:  # Adding a new object
-            default_fields = sum([list(fields) for name, fields in self.add_fieldsets], [])
-            custom_fields = [field.name for field in AppUser._meta.fields if
-                             field.name not in default_fields and field.editable and not field.many_to_many and not field.auto_created]
-            self.add_fieldsets = self.add_fieldsets + [
-                ('AppUser Fields', {'fields': custom_fields}),
-            ]
-        return form
-
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "phone_number",
+                    "date_of_birth", "is_staff", "is_superuser")
+    fields = ("first_name", "last_name", "phone_number",  "date_of_birth", "is_staff", "is_superuser", "groups")
+    list_filter = ("username", "is_staff", "is_superuser")
+    ordering = ("username", "first_name")
+    search_fields = ("username", "first_name", "last_name", "phone_number", "is_staff", "is_superuser")
